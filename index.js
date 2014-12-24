d780fd0371ff3b1cc479125d84685294df11e41c
Marcelle Wheeler <marcellewheeler@gmail.com>
2014-12-23 21:36:13 -0800

    fixed a scale bug, added a Circle object

--- index.js
+++ index.js
@@ -11,29 +11,49 @@
     })();
 
     var width, height, scale,
-        canvas, ctx,
+        canvas, context,
         particles,
         didInit;
+    var circ;
 
     function Particle(x,y){
-        this.x = x+Math.random()*32-16;
+        this.x = x+Math.random()*32-16; //note: not an int
         this.y = y+Math.random()*32-16;
     }
 
-    function update(){
-        ctx.fillStyle = 'rgba(32,32,32,0.3)';
-        ctx.fillRect(0, 0, width, height);
+    function Circle(x,y,r,vx,vy) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.vx = vx;
+        this.vy = vy;
+    }
 
-        ctx.fillStyle = 'rgba(255,255,255,1)';
+    Circle.prototype.draw = function() {
+        context.fillStyle = "#FF0000";
+        context.beginPath();
+        context.arc(this.x,this.y,this.r,0,2*Math.PI);
+        context.fill();
+    }
+
+    function update(){
+        context.fillStyle = 'rgba(32,32,32,0.3)';
+        context.fillRect(0, 0, width, height);
+        circ.draw();
+
+
+
+        context.fillStyle = 'rgba(255,255,255,1)';
         for (var i=0;i<particles.length;++i) {
             var p = particles[i];
-            ctx.fillRect(p.x, p.y, 2, 2);
+            context.fillRect(p.x, p.y, 2, 2);
         }
 
-        ctx.lineWidth=1;
-        ctx.fillStyle="#CCCCCC";
-        ctx.font="16px sans-serif";
-        ctx.fillText(""+width+"x"+height+"@"+scale+"x", 10, 25);
+        context.lineWidth=1;
+        context.fillStyle="#CCCCCC";
+        context.font="16px sans-serif";
+        context.fillText(""+width+"x"+height+"@"+scale+"x", 10, 25);
+
 
         requestAnimationFrame(update);
     }
@@ -43,10 +63,11 @@
         canvas.id = 'canvas';
         document.body.appendChild(canvas);
         resizeCanvas();
-        ctx = canvas.getContext("2d");
-        ctx.scale(scale, scale);
+        context = canvas.getContext("2d");
+        context.scale(scale, scale);
 
         particles = [];
+        circ = new Circle(width*0.5,height*0.5,50, 0, 0); //currently centred
 
         if(!didInit){
             didInit = true;
@@ -57,15 +78,17 @@
 
     function resizeCanvas(){
         scale = window.devicePixelRatio || 1;
-        canvas.width = width = window.innerWidth * scale;
-        canvas.height = height = window.innerHeight * scale;
+        width = window.innerWidth;
+        height = window.innerHeight;
+        canvas.width = window.innerWidth * scale;
+        canvas.height = window.innerHeight * scale;
         canvas.style.width = window.innerWidth + 'px';
         canvas.style.height = window.innerHeight + 'px';
     }
 
     function mouseMove(e){
         particles.push(new Particle(e.x,e.y));
-        if (particles.length > 50) {
+        if (particles.length > 10) {
             particles.shift();
         }
     }
